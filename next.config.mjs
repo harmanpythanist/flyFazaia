df1d11066e52c91bd2f5e590ab234596d6feddbe
harmanpythanist <harmanpythanist@gmail.com>
2026-02-20 01:15:49 +0500

    cachie fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {cacheComponents:true};
+const nextConfig = {cacheComponents:true,days: {
+  stale: 604800,        // 7 days — fully fresh
+  revalidate: 604800,   // 7 days — background refresh won't trigger early
+  expire: 1209600,      // 14 days — hard expiration after 2 weeks
+}
+};
 
 export default nextConfig;
